3234e85fb8311742ac1e3d70707fc71193c2393f
M Shahzad <160237343+shahzad006@users.noreply.github.com>
2024-10-03 00:34:35 +0500

    first commit

---
+++ Assigment-1 and 2/src/app/page.tsx
@@ -0,0 +1,55 @@
+
+
+import Image from 'next/image';
+import Pic from "./pic.jpg"
+
+export default function Home() {
+  return (
+
+    <div>
+    
+
+
+
+    <div className="container">
+
+      <div className="contant">
+        <p>Hi, I am <span>M Shahzad</span></p>
+        <p>Frontend Developer</p>
+      </div>
+
+
+
+
+      <div className="image">
+
+      <Image
+        src={Pic}  // Path to the image file
+        alt="Description of image"
+  
+      />
+
+
+
+        
+
+
+       
+      </div>
+      
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+  </div>
+    
+  );
+}
